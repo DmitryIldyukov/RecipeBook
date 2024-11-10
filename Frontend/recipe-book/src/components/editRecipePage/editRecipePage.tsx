897d9ba033ca52e3350901941bf34c7c6fc39f6f
DmitryIldyukov <dmitry.ildyukov@mail.ru>
2024-11-11 02:13:34 +0300

    Разбил страницу редактирования и добавления рецепта на компоненты

--- Frontend/recipe-book/src/components/editRecipePage/editRecipePage.tsx
+++ Frontend/recipe-book/src/components/editRecipePage/editRecipePage.tsx
@@ -1,24 +1,23 @@
-import { useEffect, useRef, useState } from "react";
-import MyButton from "../customComponents/myButton/myButton";
-import styles from "./editRecipePage.module.scss";
+import { useEffect, useState } from "react";
 import { useNavigate, useParams } from "react-router-dom";
-import { Ingredient, Step, Tag } from "../../types/recipe";
-import arrow from "../../assets/arrow.svg";
-import RecipeService from "../../services/recipeService";
 import { useAppStore } from "../../hooks/useStore";
-import { COOK_TIME_OPTIONS, PORTION_COUNT_OPTIONS, ROUTES } from "../../constants/constants";
-import downloadIcon from "../../assets/cloud-download.svg";
-import closeIcon from "../../assets/close.svg";
-import addPrimaryImage from "../../assets/add.svg";
-import addImage from "../../assets/add-ptimary.svg";
+import { recipeService } from "../../services/recipeService";
+import { ROUTES } from "../../constants/constants";
+import { Tag, Ingredient, Step } from "../../types/recipe";
+import MyButton from "../customComponents/myButton/myButton";
+import { RecipeForm } from "./recipeForm/recipeForm";
+import { ImageUploader } from "./imageUploader/imageUploader";
+import { IngredientsList } from "./ingredientsList/ingredientsList";
+import { StepsList } from "./stepsList/stepsList";
+import styles from "./editRecipePage.module.scss";
+import { BackBtn } from "../customComponents/backBtn/backBtn";
+import toast from "react-hot-toast";
+import { handleError } from "../../utils/errorHandler";
 
 export const EditRecipePage = () => {
-  const fileInputRef = useRef<HTMLInputElement | null>(null);
-  const recipeService = new RecipeService();
-  const navigate = useNavigate();
-
   const { recipeId } = useParams();
   const { userId } = useAppStore();
+  const navigate = useNavigate();
 
   const [name, setName] = useState<string>("");
   const [description, setDescription] = useState<string>("");
@@ -26,28 +25,29 @@ export const EditRecipePage = () => {
   const [portionCount, setPortionCount] = useState<number>(1);
   const [imageName, setImageName] = useState<string>("");
   const [imageFile, setImageFile] = useState<File | null>(null);
-
-  const [tags, setTags] = useState<Tag[]>([{ id: undefined, name: "Рецепт" }]);
+  const [tags, setTags] = useState<Tag[]>([]);
   const [ingredients, setIngredients] = useState<Ingredient[]>([{ id: undefined, title: "", description: "" }]);
   const [steps, setSteps] = useState<Step[]>([{ id: undefined, description: "" }]);
 
-  const getRecipeById = async (recipeId: string) => {
-    try {
-      const data = await recipeService.getRecipeById(parseInt(recipeId));
-      if (userId !== data.authorId) {
-        navigate(ROUTES.HOME);
-      }
-      setName(data.name);
-      setDescription(data.description);
-      setCookTime(data.cookTime);
-      setPortionCount(data.portionCount);
-      setTags(data.tags);
-      setIngredients(data.ingredients);
-      setSteps(data.steps);
-      setImageName(data.imageName);
-    } catch (error) {
-      console.error(error);
-    }
+  const getRecipeById = (recipeId: string) => {
+    recipeService
+      .getRecipeById(parseInt(recipeId))
+      .then((data) => {
+        if (userId !== data.authorId) {
+          navigate(ROUTES.HOME);
+        }
+        setName(data.name);
+        setDescription(data.description);
+        setCookTime(data.cookTime);
+        setPortionCount(data.portionCount);
+        setTags(data.tags);
+        setIngredients(data.ingredients);
+        setSteps(data.steps);
+        setImageName(data.imageName);
+      })
+      .catch((error: unknown) => {
+        console.error(error);
+      });
   };
 
   useEffect(() => {
@@ -56,12 +56,12 @@ export const EditRecipePage = () => {
     }
 
     if (recipeId) {
-      void getRecipeById(recipeId);
+      getRecipeById(recipeId);
     }
   }, [recipeId]);
 
   useEffect(() => {
-    if (imageName && recipeId) {
+    if (recipeId) {
       const fetchImage = async () => {
         try {
           const recipeImage = await recipeService.getRecipeImage(parseInt(recipeId));
@@ -75,55 +75,9 @@ export const EditRecipePage = () => {
       };
       void fetchImage();
     }
-  }, [imageName]);
-
-  const addIngredient = () => {
-    setIngredients([...ingredients, { id: undefined, title: "", description: "" }]);
-  };
-
-  const addStep = () => {
-    setSteps([...steps, { id: undefined, description: "" }]);
-  };
-
-  const removeIngredient = (index: number) => {
-    if (ingredients.length > 1) {
-      setIngredients(ingredients.filter((_, i) => i !== index));
-    }
-  };
-
-  const removeStep = (index: number) => {
-    if (steps.length > 1) {
-      setSteps(steps.filter((_, i) => i !== index));
-    }
-  };
-
-  const handleUploadClick = () => {
-    fileInputRef.current?.click();
-  };
-
-  const handleFileChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-    const file = e.target.files?.[0];
-    if (file) {
-      setImageFile(file);
-    }
-  };
-
-  const handleIngredientChange = (index: number, field: "title" | "description", value: string) => {
-    setIngredients((prevIngredients) =>
-      prevIngredients.map((ingredient, i) => (i === index ? { ...ingredient, [field]: value } : ingredient)),
-    );
-  };
-
-  const handleStepChange = (index: number, value: string) => {
-    setSteps((prevSteps) => prevSteps.map((step, i) => (i === index ? { ...step, description: value } : step)));
-  };
+  }, [recipeId]);
 
   const onSubmit = (e: React.FormEvent) => {
-    void submitForm(e);
-    navigate(ROUTES.RECIPES);
-  };
-
-  const submitForm = async (e: React.FormEvent) => {
     e.preventDefault();
 
     const formData = new FormData();
@@ -137,50 +91,47 @@ export const EditRecipePage = () => {
     formData.append("Description", description);
     formData.append("CookTime", cookTime.toString());
     formData.append("PortionCount", portionCount.toString());
+    formData.append("ImageName", imageName);
 
     if (imageFile) {
       formData.append("ImageFile", imageFile);
-      formData.append("ImageName", imageFile.name);
     }
 
     tags.forEach((tag, index) => {
-      formData.append(`Tags[${index.toString()}].Id`, tag.id?.toString() ?? "");
+      formData.append(`Tags[${index.toString()}].TagId`, tag.id?.toString() ?? "");
       formData.append(`Tags[${index.toString()}].Name`, tag.name);
     });
 
     steps.forEach((step, index) => {
-      formData.append(`Steps[${index.toString()}].Id`, step.id?.toString() ?? "");
+      formData.append(`Steps[${index.toString()}].StepId`, step.id?.toString() ?? "");
       formData.append(`Steps[${index.toString()}].Description`, step.description);
     });
 
     ingredients.forEach((ingredient, index) => {
-      formData.append(`Ingredients[${index.toString()}].Id`, ingredient.id?.toString() ?? "");
+      formData.append(`Ingredients[${index.toString()}].IngredientId`, ingredient.id?.toString() ?? "");
       formData.append(`Ingredients[${index.toString()}].Title`, ingredient.title);
       formData.append(`Ingredients[${index.toString()}].Description`, ingredient.description);
     });
 
-    try {
-      const response = recipeId
-        ? await recipeService.updateRecipe(parseInt(recipeId), formData)
-        : await recipeService.createRecipe(formData);
+    (recipeId ? recipeService.updateRecipe(parseInt(recipeId), formData) : recipeService.createRecipe(formData))
+      .then(() => {
+        toast.success("Рецепт успешно опубликован");
+        navigate(ROUTES.RECIPES);
+      })
+      .catch((error: unknown) => {
+        handleError(error, "Произошла ошибка при сохранении рецепта");
+      });
+  };
 
-      if (response.ok) {
-        console.log("Рецепт успешно опубликован!");
-      } else {
-        const errorMessages = response.json();
-        console.error("Произошла ошибка при опубликовании рецепта:", errorMessages);
-      }
-    } catch (error) {
-      console.error(error);
+  const handleFormKeyDown = (e: React.KeyboardEvent) => {
+    if (e.key === "Enter") {
+      e.preventDefault();
     }
   };
 
   return (
-    <form className={styles.container} onSubmit={onSubmit}>
-      <button className={styles.backBtn}>
-        <img src={arrow} alt="back" />
-        Назад
-      </button>
+    <form className={styles.container} onSubmit={onSubmit} onKeyDown={handleFormKeyDown}>
+      <BackBtn />
       <div className={styles.row}>
         <h3 className={styles.title}>{recipeId ? "Изменить рецепт" : "Добавить новый рецепт"}</h3>
         <MyButton
@@ -189,7 +140,7 @@ export const EditRecipePage = () => {
           height="60px"
           isOnSubmit={true}
           onClick={() => {
-            console.log("submit");
+            console.log();
           }}
         >
           Опубликовать
@@ -197,164 +148,24 @@ export const EditRecipePage = () => {
       </div>
       <div className={styles.recipeMainInfoBlock}>
         <div className={styles.card}>
-          <div className={styles.uploadInput} onClick={handleUploadClick}>
-            {imageFile && <img src={URL.createObjectURL(imageFile)} alt="recipe" className={styles.image} />}
-            <input
-              type="file"
-              accept=".png, .jpg, .jpeg"
-              id="recipeImage"
-              className={styles.hidden}
-              ref={fileInputRef}
-              onChange={handleFileChange}
-            />
-            {!imageFile && (
-              <div className={styles.uploadPlaceholder}>
-                <img src={downloadIcon} alt="download" className={styles.downloadIcon} />
-                <span className={styles.downloadText}>Загрузите фото готового блюда</span>
-              </div>
-            )}
-          </div>
-          <div className={styles.recipeInfo}>
-            <input
-              type="text"
-              placeholder="Название рецепта"
-              className={styles.inputText}
-              value={name}
-              onChange={(e) => {
-                setName(e.target.value);
-              }}
-            />
-            <textarea
-              name=""
-              id=""
-              placeholder="Краткое описание рецепта (150 символов)"
-              className={styles.area}
-              value={description}
-              onChange={(e) => {
-                setDescription(e.target.value);
-              }}
-            />
-            <input type="text" placeholder="Добавить теги" className={styles.inputText} />
-            <div className={styles.secInfoBox}>
-              <div className={`${styles.row} ${styles.gap10}`}>
-                <select
-                  value={cookTime}
-                  onChange={(e) => {
-                    setCookTime(parseInt(e.target.value));
-                  }}
-                  className={styles.selectInput}
-                >
-                  <option value="" disabled>
-                    Время готовки
-                  </option>
-                  {COOK_TIME_OPTIONS.map((option) => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-                <span className={styles.text}>Минут</span>
-              </div>
-              <div className={`${styles.row} ${styles.gap10}`}>
-                <select
-                  value={portionCount}
-                  onChange={(e) => {
-                    setPortionCount(parseInt(e.target.value));
-                  }}
-                  className={styles.selectInput}
-                >
-                  <option value="" disabled>
-                    Порций в блюде
-                  </option>
-                  {PORTION_COUNT_OPTIONS.map((option) => (
-                    <option key={option} value={option}>
-                      {option}
-                    </option>
-                  ))}
-                </select>
-                <span className={styles.text}>Персон</span>
-              </div>
-            </div>
-          </div>
+          <ImageUploader imageFile={imageFile} setImageFile={setImageFile} setImageName={setImageName} />
+          <RecipeForm
+            name={name}
+            description={description}
+            cookTime={cookTime}
+            portionCount={portionCount}
+            tags={tags}
+            setName={setName}
+            setDescription={setDescription}
+            setCookTime={setCookTime}
+            setPortionCount={setPortionCount}
+            setTags={setTags}
+          />
         </div>
-        <div className={styles.recipeSecondaryInfoBlock}>
-          <div className={styles.ingredientBlock}>
-            <p className={styles.ingredientBlockTitle}>Ингредиенты</p>
-            <ul className={styles.ingredientBlockContent}>
-              {ingredients.length > 0 &&
-                ingredients.map((ingredient, index) => (
-                  <li key={ingredient.id} className={styles.ingredientInfo}>
-                    <button
-                      type="button"
-                      className={styles.closeIconBox}
-                      onClick={() => {
-                        removeIngredient(index);
-                      }}
-                    >
-                      <img src={closeIcon} alt="close" className={styles.closeIcon} />
-                    </button>
-                    <div className={styles.ingredientData}>
-                      <input
-                        placeholder="Заголовок для ингридиентов"
-                        className={styles.ingredientTitle}
-                        value={ingredient.title}
-                        onChange={(e) => {
-                          handleIngredientChange(index, "title", e.target.value);
-                        }}
-                      />
-                      <textarea
-                        name=""
-                        id=""
-                        placeholder="Список подуктов для категории"
-                        className={styles.ingredientDescription}
-                        value={ingredient.description}
-                        onChange={(e) => {
-                          handleIngredientChange(index, "description", e.target.value);
-                        }}
-                      />
-                    </div>
-                  </li>
-                ))}
-            </ul>
-            <MyButton isPrimary={false} onClick={addIngredient} width="380px" height="60px">
-              <img src={addImage} alt="add recipe" /> Добавить заголовок
-            </MyButton>
-          </div>
-          <div className={styles.stepBlock}>
-            <ul className={styles.stepsBlock}>
-              {steps.length > 0 &&
-                steps.map((step, index) => (
-                  <li key={step.id} className={styles.stepInfo}>
-                    <div className={styles.stepTitleBox}>
-                      <p className={styles.stepTitle}>Шаг {index + 1}</p>
-                      <button
-                        type="button"
-                        className={styles.closeIconBox}
-                        onClick={() => {
-                          removeStep(index);
-                        }}
-                      >
-                        <img src={closeIcon} alt="close" className={styles.closeIcon} />
-                      </button>
-                    </div>
-                    <textarea
-                      name=""
-                      id=""
-                      placeholder="Описание шага"
-                      className={styles.stepDescription}
-                      value={step.description}
-                      onChange={(e) => {
-                        handleStepChange(index, e.target.value);
-                      }}
-                    />
-                  </li>
-                ))}
-              <MyButton isPrimary={false} onClick={addStep} width="380px" height="60px">
-                <img src={addImage} alt="add recipe" /> Добавить шаг
-              </MyButton>
-            </ul>
-          </div>
-        </div>
+      </div>
+      <div className={styles.recipeSecondaryInfoBlock}>
+        <IngredientsList ingredients={ingredients} setIngredients={setIngredients} />
+        <StepsList steps={steps} setSteps={setSteps} />
       </div>
     </form>
   );
---
+++ Frontend/recipe-book/src/components/editRecipePage/recipeForm/recipeForm.tsx
@@ -0,0 +1,97 @@
+import { COOK_TIME_OPTIONS, PORTION_COUNT_OPTIONS } from "../../../constants/constants";
+import { Tag } from "../../../types/recipe";
+import { TagEditor } from "../tagEditor/tagEditor";
+import styles from "./recipeForm.module.scss";
+import selectIcon from "../../../assets/selectArrow.svg";
+
+type RecipeFormProps = {
+  name: string;
+  description: string;
+  cookTime: number;
+  portionCount: number;
+  tags: Tag[];
+  setName: React.Dispatch<React.SetStateAction<string>>;
+  setDescription: React.Dispatch<React.SetStateAction<string>>;
+  setCookTime: React.Dispatch<React.SetStateAction<number>>;
+  setPortionCount: React.Dispatch<React.SetStateAction<number>>;
+  setTags: React.Dispatch<React.SetStateAction<Tag[]>>;
+};
+
+export const RecipeForm = ({
+  name,
+  description,
+  cookTime,
+  portionCount,
+  tags,
+  setName,
+  setDescription,
+  setCookTime,
+  setPortionCount,
+  setTags,
+}: RecipeFormProps) => {
+  return (
+    <div className={styles.recipeMainInfoBlock}>
+      <div className={styles.recipeInfo}>
+        <input
+          type="text"
+          placeholder="Название рецепта"
+          className={styles.inputText}
+          value={name}
+          onChange={(e) => {
+            setName(e.target.value);
+          }}
+        />
+        <textarea
+          placeholder="Краткое описание рецепта (150 символов)"
+          className={styles.area}
+          maxLength={150}
+          value={description}
+          onChange={(e) => {
+            setDescription(e.target.value);
+          }}
+        />
+        <TagEditor tags={tags} setTags={setTags} />
+        <div className={styles.secInfoBox}>
+          <div className={`${styles.row} ${styles.gap10}`}>
+            <select
+              value={cookTime}
+              onChange={(e) => {
+                setCookTime(parseInt(e.target.value));
+              }}
+              className={styles.selectInput}
+            >
+              <option value="" disabled>
+                Время готовки
+              </option>
+              {COOK_TIME_OPTIONS.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <span className={styles.text}>Минут</span>
+          </div>
+          <div className={`${styles.row} ${styles.gap10}`}>
+            <select
+              value={portionCount}
+              onChange={(e) => {
+                setPortionCount(parseInt(e.target.value));
+              }}
+              className={styles.selectInput}
+            >
+              <option value="" disabled>
+                Порций в блюде
+              </option>
+              {PORTION_COUNT_OPTIONS.map((option) => (
+                <option key={option} value={option}>
+                  {option}
+                </option>
+              ))}
+            </select>
+            <span className={styles.text}>Персон</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
