f1d81cd6accba01665fb8a2388636d1b6c63075f
DmitryIldyukov <dmitry.ildyukov@mail.ru>
2024-11-12 01:18:54 +0300

    Получение популярных тегов

---
+++ Backend/RecipeBook/Application/UseCases/Tags/Queries/GetPopular/GetPopularTagsQuery.cs
@@ -0,0 +1,6 @@
+﻿namespace Application.UseCases.Tags.Queries.GetPopular;
+
+public record GetPopularTagsQuery
+{
+    public int Count { get; init; }
+}
---
+++ Backend/RecipeBook/WebAPI/Mapper/TagProfile.cs
@@ -0,0 +1,13 @@
+﻿using Application.UseCases.Tags.Queries.GetPopular;
+using AutoMapper;
+using WebAPI.Dtos.Tag;
+
+namespace WebAPI.Mapper;
+
+public class TagProfile : Profile
+{
+    public TagProfile()
+    {
+        CreateMap<GetPopularTagsDto, GetPopularTagsQuery>();
+    }
+}
